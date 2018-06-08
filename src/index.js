3b9d1c5dd721774bb80545c7d3f948cebd985e05
German Jacir Molina Cabrales <gmolina@novoscale.com>
2018-06-08 10:42:36 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<div>Raffle</div>, document.getElementById('root'));
+registerServiceWorker();
