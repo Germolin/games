62c2f3ce8da1164cfde3091a9cee31b44551dd7d
German Jacir Molina Cabrales <gmolina@novoscale.com>
2018-06-12 16:00:14 -0500

    routing demo

--- src/components/tileData.js
+++ src/components/tileData.js
@@ -2,38 +2,45 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
-import StarIcon from '@material-ui/icons/Star';
 import SendIcon from '@material-ui/icons/Send';
-import MailIcon from '@material-ui/icons/Mail';
-import DeleteIcon from '@material-ui/icons/Delete';
 import ReportIcon from '@material-ui/icons/Report';
+import { Link } from 'react-router';
 
 export const mailFolderListItems = (
   <div>
-    <ListItem button>
-      <ListItemIcon>
-        <SendIcon />
-      </ListItemIcon>
-      <ListItemText primary="Send mail" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <DraftsIcon />
-      </ListItemIcon>
-      <ListItemText primary="Drafts" />
-    </ListItem>
+      <Link to={'/'}>
+        <ListItem button>
+            <ListItemIcon>
+                <SendIcon />
+            </ListItemIcon>
+            <ListItemText primary="slash" />
+        </ListItem>
+      </Link>
+
+       <Link to={'/profile'}>
+        <ListItem button>
+            <ListItemIcon>
+                <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+        </ListItem>
+      </Link>
+   
+
   </div>
 );
 
 export const otherMailFolderListItems = (
   <div>
-    <ListItem button>
-      <ListItemIcon>
-        <ReportIcon />
-      </ListItemIcon>
-      <ListItemText primary="Spam" />
-    </ListItem>
+      <Link>
+        <ListItem button>
+            <ListItemIcon>
+                <ReportIcon />
+            </ListItemIcon>
+            <ListItemText primary="Spam" />
+        </ListItem>
+      </Link>
+
   </div>
 );
